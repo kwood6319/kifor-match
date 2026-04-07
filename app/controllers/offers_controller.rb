6946eb0dc2fcf26b4cb71ea7f4725cd148a30053
Hisham <hisham.khashman@gmail.com>
2026-04-07 15:54:21 +0900

    Controller structure with TODOs

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,31 @@
 class OffersController < ApplicationController
+  # TODO: index , list all offers
+  
+  # TODO: show , show one offer details
+
+  # TODO: new , form for creating new offer
+
+  # TODO: create , presist new offer (default status = pending)
+  
+  #TODO: edit , form for editing offer
+
+  #TODO: update , update offer details
+
+  #TODO: destroy , delete offer
+
+  #TODO: approve , approve offer (only Charities can approve offers)
+
+  #TODO: reject , reject offer (only Charities can reject offers)
+
+  #TODO: search , search for offers
+
+  #TODO: update request.quantity_remaining when offer is recieved
+
+  #TODO: mark_recieved, change status to recieved (charity only)
+
+  #TODO: authorization, donor vs charity permissions
+
+  #TODO: strong params, whitelist fields
+
+  #TODO: handle succes and errors (redirects, flash messages)
 end
