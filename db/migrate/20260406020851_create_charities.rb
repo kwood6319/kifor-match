e3b06164e6b172386fd2931be8e80e78ce3c1983
Katie Wood <kwood6319@hotmail.co.uk>
2026-04-07 13:03:17 +0900

    Add null constraints to org_name and region
    Added null constraints to org_name and region fields.

--- db/migrate/20260406020851_create_charities.rb
+++ db/migrate/20260406020851_create_charities.rb
@@ -2,9 +2,9 @@ class CreateCharities < ActiveRecord::Migration[8.1]
   def change
     create_table :charities do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :org_name
+      t.string :org_name, null: false
       t.text :description
-      t.string :region
+      t.string :region, null: false
       t.string :city
       t.text :shipping_address
 
