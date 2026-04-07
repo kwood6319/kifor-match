d71fb7035e049ad158b768c688b3b4a91143fb25
Katie Wood <kwood6319@hotmail.co.uk>
2026-04-07 13:04:12 +0900

    Add null constraint to display_name in donors table

--- db/migrate/20260406020916_create_donors.rb
+++ db/migrate/20260406020916_create_donors.rb
@@ -2,7 +2,7 @@ class CreateDonors < ActiveRecord::Migration[8.1]
   def change
     create_table :donors do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :display_name
+      t.string :display_name, null: false
       t.string :donor_type
       t.string :region
       t.string :city
