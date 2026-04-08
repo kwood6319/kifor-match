a4a9a52691a64b816bb908d7f7115723d4dd90c6
Glen Gerth <glengerth3@gmail.com>
2026-04-08 11:40:56 +0900

    Display all requests as cards

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,36 @@
 class RequestsController < ApplicationController
+  def index
+    @requests = current_user.charity.requests
+  end
+
+  def show
+    # @request = Request.find(params[:id])
+  end
+
+  def new
+    # @request = Request.new
+  end
+
+  def create
+    # @request = Request.new(request_params)
+    # @request.charity = current_user.charity
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def activate
+  end
+
+  def deactivate
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:title, :category, :description, :quantity_needed, :quantity_remaining, :units, :condition, :region, :city, :urgency, :status)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :charity
+  has_one :donor
 end
