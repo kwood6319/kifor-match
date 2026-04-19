46235cf00aa425abcd668ce05b5b0eeb9ba5c503
Glen Gerth <glengerth3@gmail.com>
2026-04-19 10:54:53 +0900

    Add Pundit for Charities and Donors

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,2 +1,25 @@
 class CharitiesController < ApplicationController
+  before_action :set_charity, only: %i[approve destroy]
+
+  def index
+    @charities = policy_scope(Charity)
+  end
+
+  def approve
+    authorize @charity
+    @charity.update(approved: true)
+    redirect_to charities_path
+  end
+
+  def destroy
+    authorize @charity
+    @charity.destroy
+    redirect_to charities_path
+  end
+
+  private
+
+  def set_charity
+    @charity = Charity.find(params[:id])
+  end
 end
