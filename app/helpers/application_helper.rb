b2b75e809f5505a4f47eb9ac9419c8e35f3d37ba
Glen Gerth <glengerth3@gmail.com>
2026-05-01 20:10:47 +0900

    Fix bug and add dashboard link to navbar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def dynamic_dashboard_path(user)
+    return root_path unless user
+
+    # Check enum role and return the correct path
+    case user.role
+    when 'admin'
+      admins_dashboard_path
+    when 'charity'
+      charities_dashboard_path
+    when 'donor'
+      donors_dashboard_path
+    else
+      root_path
+    end
+  end
 end
