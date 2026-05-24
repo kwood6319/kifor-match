5726c9cd648be5da989520e57918d1247a67339b
Damien Joubert <damienusvirfortis@gmail.com>
2026-05-24 09:55:18 +0900

    back to basics...need a toggle, not a status display

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,6 @@ class DashboardController < ApplicationController
   skip_after_action :verify_authorized
 
   def show
-    @submitted_offers = Offer.where(status: "submitted").count
-    @accepted_offers = Offer.where(status: "accepted").count
-    @in_progress_offers = Offer.where(status: "accepted").count
     redirect_to helpers.dynamic_dashboard_path(current_user)
   end
 end
