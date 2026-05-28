3b8694311b56ae1c2d0beccbd07a73723b758d4c
Troy Zangara <troyzangara@gmail.com>
2026-05-28 18:01:23 +0900

    Added Approval Queue

--- app/controllers/admins/dashboards_controller.rb
+++ app/controllers/admins/dashboards_controller.rb
@@ -8,11 +8,6 @@ module Admins
       
       # Unapproved items (priority)
       @unapproved_charities = Charity.where(approved: false).order(created_at: :desc)
-      
-      # Recent items
-      @recent_charities = Charity.order(created_at: :desc).limit(3)
-      @recent_requests = Request.order(created_at: :desc).limit(3)
-      @recent_offers = Offer.order(created_at: :desc).limit(3)
     end
   end
 end
