2aa8e4feccf6b19ae9d84642faf8730f7adf85e7
Katie Wood <kwood6319@hotmail.co.uk>
2026-07-17 22:27:45 +0900

    Added model concer for categories, still more to add

---
+++ app/models/concerns/category_list.rb
@@ -0,0 +1,61 @@
+module CategoryList
+  CATEGORIES = {
+    "electronics" => {
+      icons: %w[fa-desktop fa-headphones fa-camera],
+      subcategories: %w[laptops phones headphones cameras tablets other]
+    },
+    "food" => {
+      icons: %w[fa-utensils],
+      subcategories: %w[canned fresh snacks baby_food preserved instant other]
+    },
+    "kids" => {
+      icons: %w[fa-child-reaching],
+      subcategories: %w[toys clothing school_supplies other]
+    },
+    "clothes" => {
+      icons: %w[fa-shirt],
+      subcategories: %w[mens womens childrens shoes other]
+    },
+    "hygiene" => {
+      icons: %w[fa-pump-soap],
+      subcategories: %w[]
+    },
+    "stationery" => {
+      icons: %w[fa-pen],
+      subcategories: %w[]
+    },
+    "home_goods" => {
+      icons: %w[fa-couch],
+      subcategories: %w[furniture kitchenware bedding towels other]
+    },
+    "books" => {
+      icons: %w[fa-book],
+      subcategories: %w[fiction nonfiction textbooks childrens other]
+    },
+    "other_items" => {
+      icons: %w[fa-box],
+      subcategories: %w[]
+    },
+    "cooking" => {
+      icons: %w[],
+      subcategories: %w[]
+    },
+    "cleaning" => {
+      icons: %w[],
+      subcategories: %w[]
+    },
+    "seasonal" => {
+      icons: %w[],
+      subcategories: %w[]
+    },
+    "necessities" => {
+      icons: %w[],
+      subcategories: %w[]
+    }
+
+  }.freeze
+
+  def self.label_for(key)
+    I18n.t("categories.#{key}")
+  end
+end
