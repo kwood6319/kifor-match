bb606fb3d3c2bffde02491773ecb3445f25ec8b4
Katie Wood <kwood6319@hotmail.co.uk>
2026-07-20 14:07:12 +0900

    Error fix

--- app/javascript/controllers/postage_acknowledgment_controller.js
+++ app/javascript/controllers/postage_acknowledgment_controller.js
@@ -1,9 +1,10 @@
-import { controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["checkbox", "submitButton"]
 
-  acknowledge() {
+  acknowledge(event) {
+    event.target.blur()
     this.checkboxTarget.checked = true
     this.submitButtonTarget.disabled = false
   }
