e15c31378367d3e6bb14f125e97665743631eb39
Katie Wood <kwood6319@hotmail.co.uk>
2026-08-08 19:34:03 +0900

    Adding new methods to routes, models, and locale

--- config/routes.rb
+++ config/routes.rb
@@ -75,6 +75,7 @@ Rails.application.routes.draw do
 
     get "settings", to: "settings#show", as: :settings
     patch "settings", to: "settings#update"
-    patch "settings/password", to: "settings#update_password", as: :settings_password
+    patch "settings/account", to: "settings#update_account", as: :settings_account
+    patch "settings/deactivate", to: "settings#deactivate", as: :settings_deactivate
   end
 end
