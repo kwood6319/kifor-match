ba67ff563154bc23db571845215cad9168a95343
Katie Wood <kwood6319@hotmail.co.uk>
2026-08-08 19:26:59 +0900

    Added active column to users - allows users to deactivate their account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_19_073048) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_08_102619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -122,9 +122,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_19_073048) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "locale", default: "en", null: false
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
