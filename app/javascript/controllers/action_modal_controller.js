ea6a335746d36de411fe4975f5fc7a72f3d55ca5
Alain Mimeault <lhorde@msn.com>
2026-08-12 18:59:46 +0900

    Started changes to archive modal

--- app/javascript/controllers/action_modal_controller.js
+++ app/javascript/controllers/action_modal_controller.js
@@ -1,11 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["form", "title"];
+  static targets = ["form", "title", "submit"];
   connect() {
     this.element.addEventListener("show.bs.modal", (event) => {
-      const url = event.relatedTarget?.dataset.deleteUrl;
-      const title = event.relatedTarget?.dataset.deleteTitle;
+      const url = event.relatedTarget?.dataset.actionUrl;
+      const title = event.relatedTarget?.dataset.actionTitle;
       console.log(event.relatedTarget.dataset);
       console.log(event.relatedTarget.dataset.deleteTitle);
       this.titleTarget.textContent = title;
