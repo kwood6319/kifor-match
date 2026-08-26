b932bd1cff3e2138e99b9beed2b9860c08e3c489
Alain Mimeault <lhorde@msn.com>
2026-08-26 18:31:48 +0900

    Fix button text in modal to be dynamic

--- app/javascript/controllers/action_modal_controller.js
+++ app/javascript/controllers/action_modal_controller.js
@@ -4,15 +4,13 @@ export default class extends Controller {
   static targets = ["form", "title", "submit", "method"];
   connect() {
     this.element.addEventListener("show.bs.modal", (event) => {
-      const url = event.relatedTarget?.dataset.actionUrl;
-      const title = event.relatedTarget?.dataset.actionTitle;
-      const method = event.relatedTarget?.dataset.actionMethod;
-      console.log(event.relatedTarget.dataset);
-      console.log(event.relatedTarget.dataset.deleteTitle);
-      this.titleTarget.textContent = title;
-      // document.getElementById("deleteOfferForm").action = url
-      this.formTarget.action = url;
-      this.methodTarget.value = method;
+      const { actionUrl, actionTitle, actionMethod, actionLabel } =
+        event.relatedTarget?.dataset ?? {};
+
+      this.titleTarget.textContent = actionTitle;
+      this.formTarget.action = actionUrl;
+      this.methodTarget.value = actionMethod || "delete";
+      this.submitTarget.value = actionLabel || this.submitTarget.dataset.defaultLabel;
     });
   }
 }
