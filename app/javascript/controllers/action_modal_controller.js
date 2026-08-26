9c7dff6271896b72443841512b68efc0c2516f49
Alain Mimeault <lhorde@msn.com>
2026-08-26 16:39:43 +0900

    Continued work to standardize archiving modal

--- app/javascript/controllers/action_modal_controller.js
+++ app/javascript/controllers/action_modal_controller.js
@@ -1,16 +1,18 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["form", "title", "submit"];
+  static targets = ["form", "title", "submit", "method"];
   connect() {
     this.element.addEventListener("show.bs.modal", (event) => {
       const url = event.relatedTarget?.dataset.actionUrl;
       const title = event.relatedTarget?.dataset.actionTitle;
+      const method = event.relatedTarget?.dataset.actionMethod;
       console.log(event.relatedTarget.dataset);
       console.log(event.relatedTarget.dataset.deleteTitle);
       this.titleTarget.textContent = title;
       // document.getElementById("deleteOfferForm").action = url
       this.formTarget.action = url;
+      this.methodTarget.value = method;
     });
   }
 }
