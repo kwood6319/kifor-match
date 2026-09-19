8cbb91826a844c05d3e9d868f00d5ccaf29e6f33
Katie Wood <kwood6319@hotmail.co.uk>
2026-09-19 18:14:17 +0900

    added to navigation for charities

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,18 +2,22 @@ module NavigationHelper
   def primary_nav_items(user)
     return [] unless user
 
-    items = [{ label: t("navigation.dashboard"), path: dynamic_dashboard_path(user) }]
+    [{ label: t("navigation.dashboard"), path: dynamic_dashboard_path(user) }, *role_nav_items(user)]
+  end
 
-    items << if user.admin?
-               [{ label: t("navigation.charities"), path: charities_path },
-                { label: t("navigation.donors"), path: donors_path }]
-             elsif user.donor?
-               [{ label: t("navigation.requests"), path: requests_path }]
-             else
-               []
-             end
+  private
 
-    items.flatten
+  def role_nav_items(user)
+    if user.admin?
+      [{ label: t("navigation.charities"), path: charities_path },
+       { label: t("navigation.donors"), path: donors_path }]
+    elsif user.donor?
+      [{ label: t("navigation.requests"), path: requests_path }]
+    elsif user.charity?
+      [{ label: t("navigation.history"), path: charities_archived_requests_path }]
+    else
+      []
+    end
   end
 end
 # KT To do: add unit tests for helper methods
